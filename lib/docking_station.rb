608501fe3b04e5c893c707ed979ddc82ad97f852
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-24 17:42:08 +0000

    Docking station full error underway - test failing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,14 +4,13 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    raise unless @bike
-  rescue
-    puts "There is no bike at the station"
-    @bike
+      raise "No bikes at the station" unless @bike
+      @bike
   end
 
 
   def dock(bike)
+    raise "Docking station is full" if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,20 +5,31 @@ describe DockingStation do
     expect(subject).to respond_to(:release_bike)
   end
 
-  it "should create a new bike when release_bike method is called" do
-    expect(subject.release_bike).to be_instance_of(Bike)
-  end
+#Release bike no longer creates new bike
+  # it "should create a new bike when release_bike method is called" do
+  #   expect(subject.release_bike).to be_instance_of(Bike)
+  # end
 
   it "should dock respond to the dock method" do
     expect(subject).to respond_to(:dock).with(1).argument
   end
 
-  it "should dock the bike we are giving it" do
-    bike = subject.release_bike
-    expect(subject.dock(bike)).to eq(bike)
-  end
+#Release bike no longer creates new bike
+  # it "should dock the bike we are giving it" do
+  #   bike = subject.release_bike
+  #   expect(subject.dock(bike)).to eq(bike)
+  # end
 
   it "should raise an error because there's no bike" do
     expect{subject.release_bike}.to raise_error("No bikes at the station")
   end
+
+  before do
+  @bike = Bike.new
+  end
+
+  it "should raise an error when dock is full" do
+    expect{subject.dock(Bike.new)}.to raise_error("Docking station is full")
+  end
+
 end
