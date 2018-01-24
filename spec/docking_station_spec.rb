e733444c9819f780f0fbead969bfb48c494bdd7c
Josué Estévez Fernández <jestfer6@gmail.com>
2018-01-24 12:13:37 +0000

    release bike now working, next is to check working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,11 @@
 require "docking_station"
+
 describe DockingStation do
   it "returns DockingStation to respond to the method release_bike" do
-  expect(subject).to respond_to(:release_bike)
-end
+    expect(subject).to respond_to(:release_bike)
+  end
+
+  it "should create a new bike when release_bike method is called" do
+    expect(subject.release_bike).to be_instance_of(Bike)
+  end
 end
